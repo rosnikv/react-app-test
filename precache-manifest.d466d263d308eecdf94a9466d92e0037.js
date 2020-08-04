4448978b1e6d62c06dcc40fe6d191ccc57d9938c
rosnikv <rosnikv@gmail.com>
2020-08-04 11:32:14 +0200

    Updates

---
+++ precache-manifest.d466d263d308eecdf94a9466d92e0037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b216bb55dd875126fffb78a07cc275e",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "73f62095d875cc8516db",
+    "url": "/react-app-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/react-app-test/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-test/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f62095d875cc8516db",
+    "url": "/react-app-test/static/js/main.da39bc4e.chunk.js"
+  },
+  {
+    "revision": "ba3f56ded8386e237a40",
+    "url": "/react-app-test/static/js/runtime-main.953a0489.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
